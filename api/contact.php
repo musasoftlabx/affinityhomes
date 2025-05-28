c0e4c340aed558eb7534c33226206653c4f960da
Musa Mutetwi Muliro <mmuliro@safaricom.co.ke>
2025-05-28 17:09:30 +0300

    Contact form

--- api/contact.php
+++ api/contact.php
@@ -13,11 +13,8 @@
     </html>
   ";
 
-  // To send HTML mail, the Content-type header must be set
   $headers[] = 'MIME-Version: 1.0';
   $headers[] = 'Content-type: text/html; charset=iso-8859-1';
-
-  // Additional headers
   $headers[] = 'To: Affinity Homes <info@affinityhomes.co.ke>';
   $headers[] = 'From: '.$name.' <'.$email.'>';
 
